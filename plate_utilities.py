c06802c3b26daab595ac481fd84cd7c53295e2d3
Weronika Patena <patena@gmail.com>
2013-11-26 16:31:53 -0800

    REWROTE to avoid using robotic_plate_transfer.Plate_type
    The problem was that robotic_plate_transfer.Plate_type used
    non-zero-padded well numbers!  Should probably rewrite that and merge
    them at some point...
    
    Right now 384-well plates are hardcoded - should probably allow other
    ones too.
    
    Unit-tests pass.

--- plate_utilities.py
+++ plate_utilities.py
@@ -10,15 +10,16 @@ USAGE: _____
 from __future__ import division
 import sys
 import unittest
+import string
 # other packages
 # my modules
-from robotic_plate_transfer import Plate_type
 
 PLATE_PREFIXES_REMOVE = ['plate']
+PLATE_SIZE = 384
+ROW_SIZE = 24
 
-def mutant_number_from_plate_well(plate, well, plate_size=384):
+def mutant_number_from_plate_well(plate, well):
     """ Get sequential mutant number from plate+well: first plate is 1-384, second is 385-768, etc. """
-    plate_type = Plate_type(plate_size)
     # get the plate number - we don't know if it's a string or an int, strip prefixes if string, etc
     if hasattr(plate, 'startswith'):
         for prefix in PLATE_PREFIXES_REMOVE:
@@ -26,9 +27,11 @@ def mutant_number_from_plate_well(plate, well, plate_size=384):
                 plate = plate[len(prefix):]
     plate_number = int(plate)
     # get the well number - adding 1 because the original version is 0-based
-    well_number = plate_type.get_well_number_from_ID(well) + 1
+    well_row = string.uppercase.index(well[0]) + 1
+    well_column = int(well[1:])
+    well_number = (well_row-1)*ROW_SIZE + well_column
     # get mutant ID
-    return (plate_number-1)*plate_size + well_number
+    return (plate_number-1)*PLATE_SIZE + well_number
     
 
 # TODO should probably merge this with robotic_plate_transfer.Plate_type - the unit-tests are very similar, etc...
@@ -37,20 +40,16 @@ class Testing(unittest.TestCase):
     """ Runs unit-tests for this module. """
 
     def test__mutant_number_from_plate_well(self):
-        assert mutant_number_from_plate_well(1, 'A1', 384) == 1
-        assert mutant_number_from_plate_well(1, 'A1', 384) == 1
-        assert mutant_number_from_plate_well('plate1', 'A1', 384) == 1
-        assert mutant_number_from_plate_well('plate1', 'A2', 384) == 2
-        assert mutant_number_from_plate_well('plate1', 'A24', 384) == 24
-        assert mutant_number_from_plate_well('plate1', 'B1', 384) == 25
-        assert mutant_number_from_plate_well('plate1', 'P24', 384) == 384
-        assert mutant_number_from_plate_well('plate2', 'A1', 384) == 385
-        assert mutant_number_from_plate_well('plate2', 'A2', 384) == 386
-        assert mutant_number_from_plate_well('plate3', 'A1', 384) == 769
-        assert mutant_number_from_plate_well(1, 'A1', 96) == 1
-        assert mutant_number_from_plate_well(2, 'A1', 96) == 97
-        assert mutant_number_from_plate_well(1, 'A1', 6) == 1
-        assert mutant_number_from_plate_well(2, 'A1', 6) == 7
+        assert mutant_number_from_plate_well(1, 'A1') == 1
+        assert mutant_number_from_plate_well(1, 'A1') == 1
+        assert mutant_number_from_plate_well('plate1', 'A1') == 1
+        assert mutant_number_from_plate_well('plate1', 'A2') == 2
+        assert mutant_number_from_plate_well('plate1', 'A24') == 24
+        assert mutant_number_from_plate_well('plate1', 'B1') == 25
+        assert mutant_number_from_plate_well('plate1', 'P24') == 384
+        assert mutant_number_from_plate_well('plate2', 'A1') == 385
+        assert mutant_number_from_plate_well('plate2', 'A2') == 386
+        assert mutant_number_from_plate_well('plate3', 'A1') == 769
 
 
 if __name__=='__main__':
